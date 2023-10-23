591934234834b3369c883f16407b8c8f92cebf1d
Ivan <125994256+pickless141@users.noreply.github.com>
2023-10-23 20:13:39 -0300

    update: Rutas y Controladores de Produccion

--- routes/produccionRoutes.js
+++ routes/produccionRoutes.js
@@ -3,7 +3,7 @@ const produccionesRoutes = Router();
 const produccionController = require('../controllers/produccionController/produccionController.js');
 
 // Ruta para crear una nueva producción
-produccionesRoutes.post('/', produccionController.crearProduccion);
-produccionesRoutes.get('/', produccionController.obtenerTodasLasProducciones);
+produccionesRoutes.post('/', produccionController.registrarProduccion);
+produccionesRoutes.get('/', produccionController.obtenerProducciones);
 
 module.exports = produccionesRoutes;
