35eec9990ab8cb491a41b6f6762f608d1286504c
Ivan <125994256+pickless141@users.noreply.github.com>
2023-11-22 19:15:56 -0300

    add: nuevo controlador para productos

--- controllers/productoController/productoController.js
+++ controllers/productoController/productoController.js
@@ -31,15 +31,37 @@ const obtenerTodosLosProductos = async (req, res) => {
   }
 };
 
-// Controlador para actualizar un producto por su ID
-const actualizarProductoPorId = async (req, res) => {
+// Controlador para obtener un producto por su ID
+const obtenerProductoPorId = async (req, res) => {
   const productoId = req.params.id;
-  const { nombreProducto, existencia, precio } = req.body;
 
   try {
+    const producto = await Producto.findById(productoId);
+
+    if (!producto) {
+      return res.status(404).json({ error: 'El producto no existe' });
+    }
+
+    res.status(200).json(producto);
+  } catch (error) {
+    res.status(500).json({ error: 'Error al obtener el producto' });
+  }
+};
+
+const actualizarProductoPorId = async (req, res) => {
+  const productoId = req.params.id;
+  const { nombreProducto, precio, lote } = req.body;
+
+  try {
+    const productoExistente = await Producto.findById(productoId);
+
+    if (!productoExistente) {
+      return res.status(404).json({ error: 'El producto no existe' });
+    }
+
     const producto = await Producto.findOneAndUpdate(
       { _id: productoId },
-      { nombreProducto, existencia, precio },
+      { nombreProducto, precio, lote },
       { new: true }
     );
 
@@ -53,4 +75,4 @@ const actualizarProductoPorId = async (req, res) => {
   }
 };
 
-module.exports = { crearProducto, obtenerTodosLosProductos, actualizarProductoPorId };
+module.exports = { crearProducto, obtenerTodosLosProductos, actualizarProductoPorId, obtenerProductoPorId };
