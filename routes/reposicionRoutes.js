18bef5ca989ba812201fd0ea4ef189d11fb5025e
Ivan <125994256+pickless141@users.noreply.github.com>
2023-11-22 14:45:02 -0300

    update: Controlador y ruta de reposicion

--- routes/reposicionRoutes.js
+++ routes/reposicionRoutes.js
@@ -3,7 +3,7 @@ const reposicionRoutes = Router();
 const reposicionController = require('../controllers/reposicionController/reposicionController.js'); 
 
 reposicionRoutes.post('/', reposicionController.agregarReposicion);
-reposicionRoutes.get('/', reposicionController.obtenerTodasLasReposiciones);
+reposicionRoutes.get('/', reposicionController.obtenerReposiciones);
 reposicionRoutes.get('/:id', reposicionController.buscarReposicionPorId);
 reposicionRoutes.get('/tienda/:tiendaId', reposicionController.obtenerReposicionesPorTienda);
 
